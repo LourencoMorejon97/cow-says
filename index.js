9d7a68ebbf8a3b70c17c2384b9ec94529dbfb235
Lourenço Morejón <65128813+LourencoMorejon97@users.noreply.github.com>
2022-03-16 15:36:49 +0000

    CowSays

---
+++ index.js
@@ -0,0 +1,5 @@
+const userInfo = require('./information');
+
+const cowsay = require("cowsay");
+
+console.log(cowsay.say({text:`Hi, my name is ${userInfo.firstname} and I attend ${userInfo.academy} `, e :"o0", T: "U" }));
---
+++ information.js
@@ -0,0 +1,15 @@
+const person = {
+
+    firstname: 'Lourenco',
+    academy: 'Wild Code School'
+
+
+};
+
+module.exports = { 
+    
+    firstname: person.firstname,
+
+    academy: person.academy
+}
+
